fb2e35e85f6a694495aafdca0ab0e62e5c8364cb
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-03-20 22:09:04 +0530

    kadira text added

--- server/start_cron.js
+++ server/start_cron.js
@@ -2,7 +2,7 @@ Meteor.startup(function() {
 	// try{
 	  SyncedCron.start();
 	  Meteor.setTimeout(function(){
-	  	Kadira.connect('oTQMPuDSDL3AGXf9t', '796c4b83-3b5d-4366-83cf-b1edb74091e2')	
+	  	// Kadira.connect('oTQMPuDSDL3AGXf9t', '796c4b83-3b5d-4366-83cf-b1edb74091e2')	
 	  },500);
 	// }
 	// catch(err){
