19e2eaefa4e263c4e903f9960b7f8944edf9e52f
Trinisoft Technologies <trinisofttechnologies@gmail.com>
2015-03-25 19:46:01 +0530

    t

--- client/views/posts/post_list/popup_post.js
+++ client/views/posts/post_list/popup_post.js
@@ -4,11 +4,8 @@ Template[getTemplate('popup_post')].events({
       var element = e.currentTarget;
       // var element = $(enement).parent().prev().children().val()
       var cursor = Session.get("currentPost");
-<<<<<<< HEAD
       var amount = parseInt($("#popup_post_Amount").val());
-=======
       var amount = $(element).parent().prev().children().val()
->>>>>>> 2a4f845b6715913d5b1c9f85efb332b6fe11a1f9
       var data = {};
       if(isNaN(amount))
       {
@@ -39,16 +36,13 @@ Template[getTemplate('popup_post')].events({
   'click .popup_post_Monthly': function(e, instance) {
       var element = e.currentTarget;
       var cursor = Session.get("currentPost");
-<<<<<<< HEAD
       var amount = parseInt($("#popup_post_Amount").val());
       if(isNaN(amount))
       {
         console.log(amount)
         return;
       }
-=======
       var amount = $(element).parent().prev().prev().children().val()
->>>>>>> 2a4f845b6715913d5b1c9f85efb332b6fe11a1f9
       var data = {};
       data.amount = amount;
       Meteor.call("updatePost",cursor._id,data,"Monthly",function(err,data){
